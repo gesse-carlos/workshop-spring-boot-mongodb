07930a5a112177fbe30ecc2e21d3fc3232567f7d
gesse_carlos <gesse.dev@outlook.com>
2022-06-20 05:39:56 -0300

    first commit

---
+++ src/test/java/com/api/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
